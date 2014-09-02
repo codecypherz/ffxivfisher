9b2b7ad8edcf63f3511b143fb29e5bf334551eff
James Deyerle <james.deyerle@gmail.com>
2014-09-01 20:36:09 -0600

    Remove dev filter until login is in production.

--- src/ffxiv/fisher/servlet/FishServletModule.java
+++ src/ffxiv/fisher/servlet/FishServletModule.java
@@ -3,7 +3,6 @@ package ffxiv.fisher.servlet;
 import com.google.inject.servlet.ServletModule;
 
 import ffxiv.fisher.filter.AdminFilter;
-import ffxiv.fisher.filter.DevEnvFilter;
 import ffxiv.fisher.servlet.admin.AdminFishServlet;
 import ffxiv.fisher.servlet.dev.DevLoginServlet;
 
@@ -12,7 +11,8 @@ public class FishServletModule extends ServletModule {
 	@Override
 	protected void configureServlets() {
 		// Dev only.
-		filter("/dev/*").through(DevEnvFilter.class);
+		// TODO Add this back once sign-in is in prod.
+		//filter("/dev/*").through(DevEnvFilter.class);
 		serve("/dev/login").with(DevLoginServlet.class);
 		
 		// Admin only.
