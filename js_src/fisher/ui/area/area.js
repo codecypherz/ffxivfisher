d8462d2df27a451742418af7c78975b8ddfd6933
James Deyerle <james.deyerle@gmail.com>
2014-09-06 16:35:35 -0600

    Split weather rendering for an area into its own component.

--- js_src/fisher/ui/area/area.js
+++ js_src/fisher/ui/area/area.js
@@ -5,15 +5,12 @@
 goog.provide('ff.fisher.ui.area.Area');
 
 goog.require('ff');
+goog.require('ff.fisher.ui.area.AreaWeather');
 goog.require('ff.fisher.ui.area.soy');
 goog.require('ff.fisher.ui.fish.FishRow');
-goog.require('ff.fisher.ui.weather.WeatherIcon');
-goog.require('ff.service.EorzeaTime');
 goog.require('ff.service.FishService');
 goog.require('ff.service.FishWatcher');
-goog.require('ff.service.SkywatcherService');
 goog.require('ff.ui');
-goog.require('goog.Timer');
 goog.require('goog.array');
 goog.require('goog.log');
 goog.require('goog.soy');
@@ -36,9 +33,6 @@ ff.fisher.ui.area.Area = function(area) {
   /** @private {!ff.model.Area} */
   this.area_ = area;
 
-  /** @private {!Array.<!ff.fisher.ui.weather.WeatherIcon>} */
-  this.weatherIcons_ = [];
-
   /** @private {!Array.<!ff.fisher.ui.fish.FishRow>} */
   this.fishRows_ = [];
 
@@ -48,30 +42,9 @@ ff.fisher.ui.area.Area = function(area) {
   /** @private {!ff.service.FishWatcher} */
   this.fishWatcher_ = ff.service.FishWatcher.getInstance();
 
-  /** @private {!ff.service.SkywatcherService} */
-  this.skywatcherService_ = ff.service.SkywatcherService.getInstance();
-
-  /** @private {!ff.service.EorzeaTime} */
-  this.eorzeaTime_ = ff.service.EorzeaTime.getInstance();
-
-  /** @private {Element} */
-  this.weatherList_ = null;
-
-  /** @private {Element} */
-  this.weather1_ = null;
-
-  /** @private {Element} */
-  this.weather2_ = null;
-
-  /** @private {Element} */
-  this.weather3_ = null;
-
-  /** @private {Element} */
-  this.weather4_ = null;
-
-  /** @private {!goog.Timer} */
-  this.timer_ = new goog.Timer(ff.fisher.ui.area.Area.UPDATE_INTERVAL_MS_);
-  this.registerDisposable(this.timer_);
+  /** @private {!ff.fisher.ui.area.AreaWeather} */
+  this.weather_ = new ff.fisher.ui.area.AreaWeather(this.area_);
+  this.addChild(this.weather_);
 };
 goog.inherits(ff.fisher.ui.area.Area, goog.ui.Component);
 
@@ -82,44 +55,22 @@ goog.inherits(ff.fisher.ui.area.Area, goog.ui.Component);
  */
 ff.fisher.ui.area.Area.Id_ = {
   FISH_ROWS: ff.getUniqueId('fish-rows'),
-  WEATHER_LIST: ff.getUniqueId('weather-list'),
-  WEATHER_1: ff.getUniqueId('weather-1'),
-  WEATHER_2: ff.getUniqueId('weather-2'),
-  WEATHER_3: ff.getUniqueId('weather-3'),
-  WEATHER_4: ff.getUniqueId('weather-4')
+  WEATHER: ff.getUniqueId('weather')
 };
-
-
-/**
- * @type {number}
- * @const
- * @private
- */
-ff.fisher.ui.area.Area.UPDATE_INTERVAL_MS_ = 3000;
 
 
 /** @override */
 ff.fisher.ui.area.Area.prototype.createDom = function() {
-  // Render soy template.
   this.setElementInternal(goog.soy.renderAsElement(
       ff.fisher.ui.area.soy.AREA, {
         ids: this.makeIds(ff.fisher.ui.area.Area.Id_),
         name: this.area_.getName()
       }));
 
-  this.weatherList_ = ff.ui.getElementByFragment(
-      this, ff.fisher.ui.area.Area.Id_.WEATHER_LIST);
-  this.weather1_ = ff.ui.getElementByFragment(
-      this, ff.fisher.ui.area.Area.Id_.WEATHER_1);
-  this.weather2_ = ff.ui.getElementByFragment(
-      this, ff.fisher.ui.area.Area.Id_.WEATHER_2);
-  this.weather3_ = ff.ui.getElementByFragment(
-      this, ff.fisher.ui.area.Area.Id_.WEATHER_3);
-  this.weather4_ = ff.ui.getElementByFragment(
-      this, ff.fisher.ui.area.Area.Id_.WEATHER_4);
+  this.weather_.render(ff.ui.getElementByFragment(
+      this, ff.fisher.ui.area.Area.Id_.WEATHER));
 
   this.renderFish_();
-  this.renderWeather_();
 };
 
 
@@ -137,28 +88,6 @@ ff.fisher.ui.area.Area.prototype.enterDocument = function() {
       this.fishWatcher_,
       ff.service.FishWatcher.EventType.CATCHABLE_SET_CHANGED,
       this.renderFish_);
-
-  this.getHandler().listen(
-      this.skywatcherService_,
-      ff.service.SkywatcherService.EventType.WEATHER_UPDATED,
-      this.renderWeather_);
-
-  // Listen for when to update.
-  this.getHandler().listen(
-      this.timer_,
-      goog.Timer.TICK,
-      this.updateWeatherBlocks_);
-
-  // Update regularly and right now.
-  this.timer_.start();
-  goog.Timer.callOnce(this.updateWeatherBlocks_, 50, this);
-};
-
-
-/** @override */
-ff.fisher.ui.area.Area.prototype.exitDocument = function() {
-  this.timer_.stop();
-  goog.base(this, 'exitDocument');
 };
 
 
@@ -189,128 +118,4 @@ ff.fisher.ui.area.Area.prototype.renderFish_ = function() {
 
   // Hide the component if there are no fish.
   goog.style.setElementShown(this.getElement(), fishes.length > 0);
-};
-
-
-/**
- * Updates the positions of the weather rectangles based on the current time.
- * @private
- */
-ff.fisher.ui.area.Area.prototype.updateWeatherBlocks_ = function() {
-  if (!this.isInDocument()) {
-    return;
-  }
-
-  var eorzeaDate = this.eorzeaTime_.getCurrentEorzeaDate();
-  var currentHour =
-      eorzeaDate.getUTCHours() + (eorzeaDate.getUTCMinutes() / 60.0);
-
-  var hoursUntilNextPositiveStart = this.eorzeaTime_.getHoursUntilNextHour(
-      currentHour, this.getNextWeatherChangeHour_(currentHour));
-
-  this.setLeft_(
-      this.weather1_,
-      hoursUntilNextPositiveStart - 8);
-  this.setLeft_(
-      this.weather2_,
-      hoursUntilNextPositiveStart);
-  this.setLeft_(
-      this.weather3_,
-      hoursUntilNextPositiveStart + 8);
-  this.setLeft_(
-      this.weather4_,
-      hoursUntilNextPositiveStart + 16);
-};
-
-
-/**
- * Sets the left side of the element relative to the current time assuming the
- * current time is at relative position 0.
- * @param {Element} el
- * @param {number} hoursFromLeft
- * @private
- */
-ff.fisher.ui.area.Area.prototype.setLeft_ = function(el, hoursFromLeft) {
-  var width = this.weatherList_.offsetWidth;
-  var offsetPercent = hoursFromLeft / 24.0;
-  var offsetInPixels = width * offsetPercent;
-  el.style.left = offsetInPixels + 'px';
-};
-
-
-/**
- * Renders the current weather for this area.
- * @private
- */
-ff.fisher.ui.area.Area.prototype.renderWeather_ = function() {
-  this.updateWeatherBlocks_();
-
-  var weatherList = this.skywatcherService_.getWeatherForArea(this.area_);
-  var reportHour = this.skywatcherService_.getWeatherReportHour();
-
-  // Figure out the starting index.
-  var eorzeaDate = this.eorzeaTime_.getCurrentEorzeaDate();
-  var currentHour =
-      eorzeaDate.getUTCHours() + (eorzeaDate.getUTCMinutes() / 60.0);
-
-  var nextWeatherChangeHour = this.getNextWeatherChangeHour_(currentHour);
-  var offset;
-  if (reportHour >= (nextWeatherChangeHour - 8)) {
-    offset = 0;
-  } else if (reportHour >= (nextWeatherChangeHour - 16)) {
-    offset = 1;
-  } else {
-    offset = 2;
-  }
-
-  // Clear existing weather icons.
-  goog.array.forEach(this.weatherIcons_, function(weatherIcon) {
-    this.removeChild(weatherIcon, true);
-    goog.dispose(weatherIcon);
-  }, this);
-  this.weatherIcons_ = [];
-
-  if (goog.isDefAndNotNull(weatherList)) {
-    this.renderWeatherIcon_(
-        ff.fisher.ui.area.Area.Id_.WEATHER_1, weatherList[0 + offset]);
-    this.renderWeatherIcon_(
-        ff.fisher.ui.area.Area.Id_.WEATHER_2, weatherList[1 + offset]);
-    this.renderWeatherIcon_(
-        ff.fisher.ui.area.Area.Id_.WEATHER_3, weatherList[2 + offset]);
-    this.renderWeatherIcon_(
-        ff.fisher.ui.area.Area.Id_.WEATHER_4, weatherList[3 + offset]);
-  }
-};
-
-
-/**
- * @param {number} currentHour
- * @return {number}
- * @private
- */
-ff.fisher.ui.area.Area.prototype.getNextWeatherChangeHour_ = function(
-    currentHour) {
-  if (currentHour < 8) {
-    return 8;
-  } else if (currentHour < 16) {
-    return 16;
-  }
-  return 24;
-};
-
-
-/**
- * @param {!ff.fisher.ui.area.Area.Id_} id
- * @param {ff.model.Weather} weather
- * @private
- */
-ff.fisher.ui.area.Area.prototype.renderWeatherIcon_ = function(id, weather) {
-  if (!weather) {
-    return;
-  }
-  var weatherIcon = new ff.fisher.ui.weather.WeatherIcon(weather);
-  var container = ff.ui.getElementByFragment(this, id);
-  this.addChild(weatherIcon);
-  this.weatherIcons_.push(weatherIcon);
-  weatherIcon.render(container);
 };
---
+++ js_src/fisher/ui/area/areaweather.js
@@ -0,0 +1,255 @@
+/**
+ * Renders fish for a single area.
+ */
+
+goog.provide('ff.fisher.ui.area.AreaWeather');
+
+goog.require('ff');
+goog.require('ff.fisher.ui.area.soy');
+goog.require('ff.fisher.ui.weather.WeatherIcon');
+goog.require('ff.service.EorzeaTime');
+goog.require('ff.service.SkywatcherService');
+goog.require('ff.ui');
+goog.require('goog.Timer');
+goog.require('goog.array');
+goog.require('goog.log');
+goog.require('goog.soy');
+goog.require('goog.ui.Component');
+
+
+
+/**
+ * @param {!ff.model.Area} area
+ * @constructor
+ * @extends {goog.ui.Component}
+ */
+ff.fisher.ui.area.AreaWeather = function(area) {
+  goog.base(this);
+
+  /** @protected {goog.log.Logger} */
+  this.logger = goog.log.getLogger('ff.fisher.ui.area.AreaWeather');
+
+  /** @private {!ff.model.Area} */
+  this.area_ = area;
+
+  /** @private {!Array.<!ff.fisher.ui.weather.WeatherIcon>} */
+  this.weatherIcons_ = [];
+
+  /** @private {!ff.service.SkywatcherService} */
+  this.skywatcherService_ = ff.service.SkywatcherService.getInstance();
+
+  /** @private {!ff.service.EorzeaTime} */
+  this.eorzeaTime_ = ff.service.EorzeaTime.getInstance();
+
+  /** @private {Element} */
+  this.weather1_ = null;
+
+  /** @private {Element} */
+  this.weather2_ = null;
+
+  /** @private {Element} */
+  this.weather3_ = null;
+
+  /** @private {Element} */
+  this.weather4_ = null;
+
+  /** @private {!goog.Timer} */
+  this.timer_ = new goog.Timer(
+      ff.fisher.ui.area.AreaWeather.UPDATE_INTERVAL_MS_);
+  this.registerDisposable(this.timer_);
+};
+goog.inherits(ff.fisher.ui.area.AreaWeather, goog.ui.Component);
+
+
+/**
+ * @enum {string}
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.Id_ = {
+  WEATHER_1: ff.getUniqueId('weather-1'),
+  WEATHER_2: ff.getUniqueId('weather-2'),
+  WEATHER_3: ff.getUniqueId('weather-3'),
+  WEATHER_4: ff.getUniqueId('weather-4')
+};
+
+
+/**
+ * @type {number}
+ * @const
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.UPDATE_INTERVAL_MS_ = 3000;
+
+
+/** @override */
+ff.fisher.ui.area.AreaWeather.prototype.createDom = function() {
+  this.setElementInternal(goog.soy.renderAsElement(
+      ff.fisher.ui.area.soy.AREA_WEATHER, {
+        ids: this.makeIds(ff.fisher.ui.area.AreaWeather.Id_)
+      }));
+
+  this.weather1_ = ff.ui.getElementByFragment(
+      this, ff.fisher.ui.area.AreaWeather.Id_.WEATHER_1);
+  this.weather2_ = ff.ui.getElementByFragment(
+      this, ff.fisher.ui.area.AreaWeather.Id_.WEATHER_2);
+  this.weather3_ = ff.ui.getElementByFragment(
+      this, ff.fisher.ui.area.AreaWeather.Id_.WEATHER_3);
+  this.weather4_ = ff.ui.getElementByFragment(
+      this, ff.fisher.ui.area.AreaWeather.Id_.WEATHER_4);
+
+  this.renderWeather_();
+};
+
+
+/** @override */
+ff.fisher.ui.area.AreaWeather.prototype.enterDocument = function() {
+  goog.base(this, 'enterDocument');
+
+  // Listen for weather updates.
+  this.getHandler().listen(
+      this.skywatcherService_,
+      ff.service.SkywatcherService.EventType.WEATHER_UPDATED,
+      this.renderWeather_);
+
+  // Update occasionally, but a little slower than time ticks.
+  this.getHandler().listen(
+      this.timer_,
+      goog.Timer.TICK,
+      this.updateWeatherBlocks_);
+
+  // Update regularly and right now.
+  this.timer_.start();
+  goog.Timer.callOnce(this.updateWeatherBlocks_, 50, this);
+};
+
+
+/** @override */
+ff.fisher.ui.area.AreaWeather.prototype.exitDocument = function() {
+  this.timer_.stop();
+  goog.base(this, 'exitDocument');
+};
+
+
+/**
+ * Updates the positions of the weather rectangles based on the current time.
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.prototype.updateWeatherBlocks_ = function() {
+  if (!this.isInDocument()) {
+    return;
+  }
+
+  var eorzeaDate = this.eorzeaTime_.getCurrentEorzeaDate();
+  var currentHour =
+      eorzeaDate.getUTCHours() + (eorzeaDate.getUTCMinutes() / 60.0);
+
+  var hoursUntilNextPositiveStart = this.eorzeaTime_.getHoursUntilNextHour(
+      currentHour, this.getNextWeatherChangeHour_(currentHour));
+
+  this.setLeft_(
+      this.weather1_,
+      hoursUntilNextPositiveStart - 8);
+  this.setLeft_(
+      this.weather2_,
+      hoursUntilNextPositiveStart);
+  this.setLeft_(
+      this.weather3_,
+      hoursUntilNextPositiveStart + 8);
+  this.setLeft_(
+      this.weather4_,
+      hoursUntilNextPositiveStart + 16);
+};
+
+
+/**
+ * Sets the left side of the element relative to the current time assuming the
+ * current time is at relative position 0.
+ * @param {Element} el
+ * @param {number} hoursFromLeft
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.prototype.setLeft_ = function(el, hoursFromLeft) {
+  var width = this.getElement().offsetWidth;
+  var offsetPercent = hoursFromLeft / 24.0;
+  var offsetInPixels = width * offsetPercent;
+  el.style.left = offsetInPixels + 'px';
+};
+
+
+/**
+ * Renders the current weather for this area.
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.prototype.renderWeather_ = function() {
+  this.updateWeatherBlocks_();
+
+  var weatherList = this.skywatcherService_.getWeatherForArea(this.area_);
+  var reportHour = this.skywatcherService_.getWeatherReportHour();
+
+  // Figure out the starting index.
+  var eorzeaDate = this.eorzeaTime_.getCurrentEorzeaDate();
+  var currentHour =
+      eorzeaDate.getUTCHours() + (eorzeaDate.getUTCMinutes() / 60.0);
+
+  var nextWeatherChangeHour = this.getNextWeatherChangeHour_(currentHour);
+  var offset;
+  if (reportHour >= (nextWeatherChangeHour - 8)) {
+    offset = 0;
+  } else if (reportHour >= (nextWeatherChangeHour - 16)) {
+    offset = 1;
+  } else {
+    offset = 2;
+  }
+
+  // Clear existing weather icons.
+  goog.array.forEach(this.weatherIcons_, function(weatherIcon) {
+    this.removeChild(weatherIcon, true);
+    goog.dispose(weatherIcon);
+  }, this);
+  this.weatherIcons_ = [];
+
+  if (goog.isDefAndNotNull(weatherList)) {
+    this.renderWeatherIcon_(
+        ff.fisher.ui.area.AreaWeather.Id_.WEATHER_1, weatherList[0 + offset]);
+    this.renderWeatherIcon_(
+        ff.fisher.ui.area.AreaWeather.Id_.WEATHER_2, weatherList[1 + offset]);
+    this.renderWeatherIcon_(
+        ff.fisher.ui.area.AreaWeather.Id_.WEATHER_3, weatherList[2 + offset]);
+    this.renderWeatherIcon_(
+        ff.fisher.ui.area.AreaWeather.Id_.WEATHER_4, weatherList[3 + offset]);
+  }
+};
+
+
+/**
+ * @param {number} currentHour
+ * @return {number}
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.prototype.getNextWeatherChangeHour_ = function(
+    currentHour) {
+  if (currentHour < 8) {
+    return 8;
+  } else if (currentHour < 16) {
+    return 16;
+  }
+  return 24;
+};
+
+
+/**
+ * @param {!ff.fisher.ui.area.AreaWeather.Id_} id
+ * @param {ff.model.Weather} weather
+ * @private
+ */
+ff.fisher.ui.area.AreaWeather.prototype.renderWeatherIcon_ = function(
+    id, weather) {
+  if (!weather) {
+    return;
+  }
+  var weatherIcon = new ff.fisher.ui.weather.WeatherIcon(weather);
+  var container = ff.ui.getElementByFragment(this, id);
+  this.addChild(weatherIcon);
+  this.weatherIcons_.push(weatherIcon);
+  weatherIcon.render(container);
+};
