76a1212eaa8d17e5dd89baa4d633295efa0e74a6
James Deyerle <james.deyerle@gmail.com>
2014-09-20 08:53:01 -0600

    Removed a temporary fish.

--- src/ffxiv/fisher/servlet/FishesServlet.java
+++ src/ffxiv/fisher/servlet/FishesServlet.java
@@ -1,7 +1,6 @@
 package ffxiv.fisher.servlet;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,18 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import ffxiv.fisher.model.CatchPathPart;
 import ffxiv.fisher.model.Fish;
-import ffxiv.fisher.model.FishingTackle;
-import ffxiv.fisher.model.Location;
-import ffxiv.fisher.model.Mooch;
-import ffxiv.fisher.model.StraightCatch;
-import ffxiv.fisher.model.Weather;
 import ffxiv.fisher.service.FishService;
 
 /**
@@ -42,16 +34,6 @@ public class FishesServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		List<Fish> allFish = fishService.getAll();
-		allFish.add(new Fish(
-				"key",
-				"fake name",
-				new HashSet<Weather>(),
-				0,
-				23,
-				Location.BLIND_IRON_MINES,
-				ImmutableList.<CatchPathPart>of(
-						new StraightCatch(FishingTackle.MOTH_PUPA),
-						new Mooch("some fish to mooch"))));
 		Gson gson = new Gson();
 		resp.getWriter().write(gson.toJson(allFish));
 	}
