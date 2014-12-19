466f7d3c47c53d6d752be56b001d0a5d05a36a04
codecypher <james.deyerle@gmail.com>
2014-12-18 21:10:02 -0700

    Fixed some paths.

--- scripts/run.bat
+++ scripts/run.bat
@@ -13,7 +13,7 @@ echo ___________________________________________
 echo Running FFXIV Fisher locally...
 echo.
 
-"C:\Program Files\eclipse luna\plugins\com.google.appengine.eclipse.sdkbundle_1.9.17\appengine-java-sdk-1.9.17\bin\dev_appserver.cmd" ^
+"C:\Program Files\eclipse\plugins\com.google.appengine.eclipse.sdkbundle_1.9.17\appengine-java-sdk-1.9.17\bin\dev_appserver.cmd" ^
    --port=8888 ^
    --jvm_flag=-Xdebug ^
    --jvm_flag=-Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n ^
